8a105bfac3580dd82eaebf3f75d31fd41b81e108
Caitlin Bannan <bannanc@users.noreply.github.com>
2017-04-05 14:26:19 -0700

    rename to smirnoff
    Another note, SMIRKS is already an acronym, it should be capitalized anywhere in our documentation. I'm trying to keep an eye out and fix it when I see smirks or Smirks.

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 """
-Setup script to install the smirff99Frosst.ffxml as a python package
+Setup script to install the smirnoff99Frosst.ffxml as a python package
 """
 
 import sys,os
@@ -12,19 +12,19 @@ if sys.argv[-1] == 'setup.py':
     print()
 
 descr = """
-This provides the first general-purpose implementation of a Smirks Force Field (SMIRFF) as implemented by SMARTY and its ForceField class (in smarty.forcefield) for parameterizing small molecules for OpenMM.
+This provides the first general-purpose implementation of a SMIRKS Native Open Force Field (SMIRNOFF) as implemented by SMARTY and its ForceField class (in smarty.forcefield) for parameterizing small molecules for OpenMM.
 """
 
 setup(
-    name                 = 'smirff99frosst',
+    name                 = 'smirnoff99frosst',
     version              = '1.0.5',
     description          = 'SMIRNOFF Forcefield parameters',
     long_description     = descr,
-    url                  = 'https://github.com/open-forcefield-group/smirff99Frosst',
+    url                  = 'https://github.com/open-forcefield-group/smirnoff99Frosst',
     author               = 'Christopher I. Bayly, Caitlin C. Bannan, David L. Mobley',
     author_email         = 'dmobley@uci.edu',
     license              = 'MIT',
     platforms            = ['Linux-64', 'Mac OSX-64', 'Unix-64'],
-    packages             = find_packages()+['smirff99frosst'],
+    packages             = find_packages()+['smirnoff99frosst'],
     include_package_data = True
 )
