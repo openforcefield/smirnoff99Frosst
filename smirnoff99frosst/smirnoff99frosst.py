b0368f7d2eb4261fe877a7f6a12fa58645981eac
Matthew W. Thompson <mattwthompson@protonmail.com>
2024-04-08 09:00:40 -0500

    Drop use of `pkg_resources`

--- smirnoff99frosst/smirnoff99frosst.py
+++ smirnoff99frosst/smirnoff99frosst.py
@@ -3,26 +3,25 @@ smirnoff99frosst.py
 A general small molecule forcefield descended from AMBER99 and parm@Frosst in the SMIRNOFF format.
 
 This module only contains the function that will be the entry point that
-will be used by the openforcefield toolkit to find the installed forcefield
-files.
+will be used by the OpenFF Toolkit to find the installed forcefield files.
 
 """
 
-from pkg_resources import resource_filename
+from importlib.resources import files
 
 
-def get_forcefield_dirs_paths():
+def get_forcefield_dirs_paths() -> list[str]:
     """
     Return the paths to the directories including the forcefield files.
 
     This function is set as an entry point in setup.py. It will be called
-    by the openforcefield toolkit when discovering the installed folders
+    by the OpenFF Toolkit when discovering the installed folders
     including offxml files.
 
     Returns
     -------
-    dir_paths : List[str]
+    dir_paths : list[str]
         The list of directory paths containing the SMIRNOFF files.
 
     """
-    return [resource_filename('smirnoff99frosst', 'offxml')]
+    return [(files("smirnoff99frosst") / "offxml").as_posix()]
